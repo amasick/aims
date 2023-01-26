c71afc5e37c4f7e5ef6592d9517d7bd1390df962
shyam <shyamprajapati2672@gmail.com>
2023-01-26 22:46:09 +0530

    fsd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DashBoard from "./pages/dashboard";
 import Login from "./pages/login";
 import Register from "./pages/register";
-
+import Header  from './components/Headers';
 function App() {
   return (
 <>
 <Router>
 <div className="container">
+  <Header/>
 <Routes>
 <Route path="/" element={<DashBoard/>}/>
 <Route path="/login" element={<Login/>}/>
